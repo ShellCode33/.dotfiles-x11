d163ed86f8bbe1333ffd594f3ea5753549542a2e
ShellCode33 <shellcode33@protonmail.ch>
2021-12-17 01:49:59 +0100

    Fix and improve hdmi_auto_switch.sh

--- .local/bin/hdmi_auto_switch.sh
+++ .local/bin/hdmi_auto_switch.sh
@@ -5,7 +5,7 @@ set -x
 # NOTE : to run this script automatically when a HDMI device is plugged in, add the
 #        following line to /lib/udev/rules.d/99-hdmi_sound.rules
 #
-#        ACTION=="change", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/USERNAME/.Xauthority", RUN+="/path/to/hdmi_auto_switch.sh"
+#        ACTION=="change", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/USERNAME/.Xauthority", RUN+="/usr/bin/su USERNAME -c /path/to/hdmi_auto_switch.sh"
 #
 
 export DISPLAY=:0
@@ -18,15 +18,18 @@ ALSA_DEVICE="$(pactl list cards | grep Name | head -1 | cut -d. -f2,3)"
 if [ -n "$SECONDARY_DISPLAY" ]; then
     PROFILE="hdmi-stereo-extra1"
     xrandr --output "$SECONDARY_DISPLAY" --auto --scale-from 1920x1080 --output "$PRIMARY_DISPLAY"
+    notify-send 'HDMI output detected'
 else
     PROFILE="analog-stereo"
+    xrandr --auto
+    notify-send 'HDMI cable plugged out'
 fi
 
-pactl set-card-profile alsa_card.$ALSA_DEVICE output:$PROFILE
-pactl set-default-sink alsa_output.$ALSA_DEVICE.$PROFILE
+pactl set-card-profile "alsa_card.${ALSA_DEVICE} output:${PROFILE}"
+pactl set-default-sink "alsa_output.${ALSA_DEVICE}.${PROFILE}"
 
-SINK_INPUTS=($(pactl list sink-inputs | grep "^Sink Input #"|cut -d'#' -f2))
-for i in ${SINK_INTPUTS[*]}; do
-    pactl move-sink-input $i alsa_output.$ALSA_DEVICE.$PROFILE
+SINK_INPUTS=($(pactl list sink-inputs | grep "^Sink Input #" | cut -d'#' -f2))
+for i in ${SINK_INPUTS[*]}; do
+    pactl move-sink-input "$i" "alsa_output.${ALSA_DEVICE}.${PROFILE}"
 done
 
