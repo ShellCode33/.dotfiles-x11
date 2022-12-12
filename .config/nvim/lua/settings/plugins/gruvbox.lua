06953c3d1998395109922c8546599a615b4278d5
ShellCode33 <shellcode33@protonmail.ch>
2022-12-12 17:29:38 +0100

    nvim : disable italication of operators to prevent confusion between | and /

--- .config/nvim/lua/settings/plugins/gruvbox.lua
+++ .config/nvim/lua/settings/plugins/gruvbox.lua
@@ -1,2 +1,10 @@
 vim.o.background = "dark"
+
+require("gruvbox").setup({
+    overrides = {
+        -- | operator could be confused with / when using italic
+        Operator = {italic = false}
+    }
+})
+
 vim.cmd([[colorscheme gruvbox]])
